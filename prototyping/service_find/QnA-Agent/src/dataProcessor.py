9c7a04c7c7de057e3bfe17d60b3a2e4a7b17f982
lewiEyasu <eyasulewi@gmail.com>
2023-10-18 12:01:20 +0300

    feat: Add encoding and constants
    Add the encoding definition and constants used in the code.

--- prototyping/service_find/QnA-Agent/src/dataProcessor.py
+++ prototyping/service_find/QnA-Agent/src/dataProcessor.py
@@ -1,16 +1,27 @@
+import concurrent.futures
 import io
+import json
 import os
-from concurrent.futures import ThreadPoolExecutor
-from glob import glob
+import pathlib
+import pandas as pd
 import re
 import requests
-from pathlib import Path
+import tiktoken
 import zipfile
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+from glob import glob
+from markdown import markdown
+from pathlib import Path
 import markdown
-import pandas as pd
-import json
-import tiktoken
+
+ENCODING = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+CHUNK_SIZE = 200  # The target size of each text chunk in tokens
+MIN_CHUNK_SIZE_CHARS = 350  # The minimum size of each text chunk in characters
+MIN_CHUNK_LENGTH_TO_EMBED = 5  # Discard chunks shorter than this
+MAX_NUM_CHUNKS = 300  # The maximum number of chunks to generate from a text
+ENDPAGE_NUM = 282  # the last page number
 
 
 def download_files(url: str, local_directory: str) -> None:
