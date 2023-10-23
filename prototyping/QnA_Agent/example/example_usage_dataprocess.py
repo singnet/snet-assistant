2cf2eb96ae7ea46523f3eb0a22e9fb44885f10a4
lewiEyasu <eyasulewi@gmail.com>
2023-10-23 17:28:56 +0300

    render the code

--- prototyping/QnA_Agent/example/example_usage_dataprocess.py
+++ prototyping/QnA_Agent/example/example_usage_dataprocess.py
@@ -1,11 +1,9 @@
-from prototyping.QnA_Agent.src import dataProcessor
+# # Define the URL of the ZIP archive
+# repo_url = 'https://github.com/singnet/dev-portal/archive/refs/heads/master.zip'
 
+# # Create an instance of DataProcessor
+# processor = src.read_json(repo_url)
 
-# Define the URL of the ZIP archive
-repo_url = 'https://github.com/singnet/dev-portal/archive/refs/heads/master.zip'
-
-# Create an instance of DataProcessor
-processor = dataProcessor.DataProcessor(repo_url)
-
-# The DataProcessor class constructor will automatically download the files,
-# process them, save to CSV, and delete the 'dev-portal-master' directory.
+# # The DataProcessor class constructor will automatically download the files,
+# # process them, save to CSV, and delete the 'dev-portal-master' directory.
+# import src
