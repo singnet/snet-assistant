118be6cb6c8e776cd0b70893a3a7d52a900b0d4a
sveta <bessveta>
2023-12-02 00:21:23 +0300

    add call for dialogue agent

--- prototyping/metta_llm/metta_guidance/runner.py
+++ prototyping/metta_llm/metta_guidance/runner.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import hyperon as hp
 
@@ -12,8 +13,10 @@ if __name__ == '__main__':
      use services short descriptions as prompt to get service which solves user's task
     '''
     metta_motto_path = os.environ["METTAMOTOPATH"]
+
+
     env_builder = hp.Environment.custom_env(include_paths=[metta_motto_path])
     metta = hp.MeTTa(env_builder=env_builder)
-    run_script("prototyping/metta_llm/metta_guidance/question_types_guidance.metta", metta)
+    run_script("prototyping/metta_llm/metta_dialogue_agent/test_llm_agent.metta", metta)
 
 
